39bbac64da91750a07ced616be50d99b1ee3bd8e
MargotGuo <gyqgdn@126.com>
2020-04-07 18:35:02 +0800

    [Guo Yuanqi]{refactor}

--- Week-01-02-basic-student_management_system/src/main/java/com/thoughtworks/educationSys/controllers/StudentController.java
+++ Week-01-02-basic-student_management_system/src/main/java/com/thoughtworks/educationSys/controllers/StudentController.java
@@ -8,51 +8,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @RestController
 public class StudentController {
 
+  private final StudentRepository studentRepository = new StudentRepository();
+
   @GetMapping("/student/get/{name}")
   public Student getStudent(@PathVariable String name) {
-    List<Student> matchedStudent = StudentRepository.getStudentList()
-        .stream()
-        .filter(student -> student.getName().equals(name))
-        .collect(Collectors.toList());
-    if (matchedStudent.isEmpty()) {
-      return null;
-    }
-    return matchedStudent.get(0);
+    return studentRepository.getStudent(name).orElse(null);
   }
 
   @GetMapping("/student/get/all")
   public Iterable<Student> getAllStudent() {
-    return StudentRepository.getStudentList();
+    return studentRepository.getStudentList();
   }
 
   @PostMapping("student/insert")
   public String insertStudent(@RequestBody Student student) {
-    boolean isDuplicateName = StudentRepository.getStudentList()
-        .stream()
-        .anyMatch(student1 -> student1.getName().equals(student.getName()));
-    if (isDuplicateName) {
-      return "姓名重复";
+    boolean hasStudent = studentRepository.hasStudent(student.getName());
+    if (!hasStudent) {
+      studentRepository.add(student);
     }
-    StudentRepository.getStudentList().add(student);
-    return "添加成功";
+    return hasStudent ? "姓名重复" : "添加成功";
   }
 
   @PostMapping("student/delete/{name}")
   public String deleteStudent(@PathVariable String name) {
-    List<Student> matchedStudent = StudentRepository.getStudentList()
-        .stream()
-        .filter(student -> student.getName().equals(name))
-        .collect(Collectors.toList());
-    if (matchedStudent.isEmpty()) {
-      return "该学生不存在";
+    boolean hasStudent = studentRepository.hasStudent(name);
+    if (hasStudent) {
+      studentRepository.delete(name);
     }
-    StudentRepository.getStudentList().remove(matchedStudent.get(0));
-    return "删除成功";
+    return hasStudent ? "删除成功" : "该学生不存在";
   }
 }
--- Week-01-02-basic-student_management_system/src/main/java/com/thoughtworks/educationSys/repository/StudentRepository.java
+++ Week-01-02-basic-student_management_system/src/main/java/com/thoughtworks/educationSys/repository/StudentRepository.java
@@ -4,16 +4,39 @@ import com.thoughtworks.educationSys.entities.Student;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class StudentRepository {
-  private static List<Student> studentList = new ArrayList<>();
+  private List<Student> studentList = new ArrayList<>();
 
-  static {
+  {
     studentList.add(new Student("Lily", "FEMALE", "1-1"));
     studentList.add(new Student("Mike", "MALE", "1-3"));
   }
 
-  public static List<Student> getStudentList() {
+  public List<Student> getStudentList() {
     return studentList;
+  }
+
+  public Optional<Student> getStudent(String name) {
+    return getStudentList()
+        .stream()
+        .filter(student -> student.getName().equals(name))
+        .findAny();
+  }
+
+  public void add(Student student) {
+    studentList.add(student);
+  }
+
+  public void delete(String name) {
+    Optional<Student> student = getStudent(name);
+    student.ifPresent(student1 -> studentList.remove(student1));
+  }
+
+  public boolean hasStudent(String name) {
+    Optional<Student> student = getStudent(name);
+    return student.isPresent();
   }
 }
